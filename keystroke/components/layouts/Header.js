8622595032af6b92d337ba8eca0db6ec81d8a726
Gauranshi Gupta <gauranshigupta2000@gmail.com>
2022-11-28 12:17:41 +0530

    login button

--- keystroke/components/layouts/Header.js
+++ keystroke/components/layouts/Header.js
@@ -183,16 +183,12 @@ const Header = ({ headerSetting = {} }) => {
                             </a>
                           </li>
                           <li>
-                            {/* <a href="#" className="login">
-                                                            Login
-                                                        </a> */}
-                            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
-                              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                <a href="#" className="login">
-                                  Login
-                                </a>
-                              </span>
-                            </button>
+                            <a
+                              href="#"
+                              className="axil-button btn-large btn-transparent rounded-lg"
+                            >
+                              Login
+                            </a>
                           </li>
                         </ul>
                       </div>
